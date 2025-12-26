68899f3b6a91639f12f864e8ac3ace3ce9c73b2e
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-27 02:30:48 +0530

    Enhance WelcomePage layout with logo and title text

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -7,11 +8,37 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/getStart.jpeg'),
             fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 120),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset(
+                'assets/logo.jpg',
+                width: 100,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Text(
+              "Find your Favorite",
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
