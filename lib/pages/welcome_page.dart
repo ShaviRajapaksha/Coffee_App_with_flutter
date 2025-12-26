bfc04d2a9c09a2135301024606f32ee20c74269d
IT20272654 <shavindurajapaksha@gmail.com>
2025-12-27 03:03:51 +0530

    Enhance WelcomePage with additional text and styled button for better user engagement

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -36,7 +36,68 @@ class WelcomePage extends StatelessWidget {
                 fontSize: 30,
                 fontFamily: GoogleFonts.poppins().fontFamily,
                 color: const Color(0xFF9a7558),
-                fontWeight: FontWeight.bold,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              "Coffee Taste!",
+              style: TextStyle(
+                fontSize: 40,
+                fontFamily: GoogleFonts.poppins().fontFamily,
+                color: const Color(0xFF9a7558),
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Text(
+                '''We're here to help you find the best coffee in town.''',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: GoogleFonts.poppins().fontFamily,
+                  color: const Color(0xFF5f3c26),
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  backgroundColor: const Color(0xFF9a7558),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 15,
+                  ),
+                ),
+                onPressed: () {},
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        "Get Started",
+                      ),
+                      Icon(
+                        //Icons.Color.white,
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
